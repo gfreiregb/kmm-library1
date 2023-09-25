ea592ed55a53acaf1b0a8731f324d80b334c5419
Guilherme Freire <guilherme.freire@globant.com>
2023-09-25 13:40:55 -0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+rootProject.name = "kmm_library1"
+include(":shared")
+include(":androidsampleapp")
